01c859dd144ee692947dbe7a979a81d2d2547bfa
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-06-18 09:07:08 +0200

    Refactor MainScreen layout for improved readability and loading state handling

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -25,57 +25,63 @@ class _MainScreenState extends State<MainScreen> {
         title: Text('API Tasks'),
       ),
       body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Cocktailname:"),
-            isLoading ? CircularProgressIndicator() : SizedBox.shrink(),
-            Text(drinkName,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                )),
-            SizedBox(height: 20),
-            Text("mit oder ohne Alkohol?:"),
-            isLoading ? CircularProgressIndicator() : SizedBox.shrink(),
-            Text(alcoholic,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                )),
-            SizedBox(height: 20),
-            Image.network(image, height: 300, width: 300, fit: BoxFit.cover),
-            isLoading ? CircularProgressIndicator() : SizedBox.shrink(),
-            SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  isLoading = true;
-                });
-                final response = await http.get(
-                  Uri.parse(
-                      "https://www.thecocktaildb.com/api/json/v1/1/random.php"),
-                );
-                String myJson = response.body;
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: isLoading
+              ? CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Cocktailname:"),
+                    Text(
+                      drinkName,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text("mit oder ohne Alkohol?:"),
+                    Text(
+                      alcoholic,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Image.network(image,
+                        height: 300, width: 300, fit: BoxFit.cover),
+                    SizedBox(height: 50),
+                    ElevatedButton(
+                      onPressed: () async {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        final response = await http.get(
+                          Uri.parse(
+                              "https://www.thecocktaildb.com/api/json/v1/1/random.php"),
+                        );
+                        String myJson = response.body;
 
-                final Map<String, dynamic> myDrinks = jsonDecode(myJson);
-                setState(() {
-                  drinkName =
-                      myDrinks["drinks"]?[0]?["strDrink"] ?? "No drink found";
-                  alcoholic = myDrinks["drinks"]?[0]?["strAlcoholic"] ??
-                      "No alcoholic information found";
-                  image = myDrinks["drinks"]?[0]?["strDrinkThumb"] ??
-                      "No image found";
-                  isLoading = false;
-                });
-              },
-              child: Text("click for new Cocktail"),
-            )
-          ],
+                        final Map<String, dynamic> myDrinks =
+                            jsonDecode(myJson);
+                        setState(() {
+                          drinkName = myDrinks["drinks"]?[0]?["strDrink"] ??
+                              "No drink found";
+                          alcoholic = myDrinks["drinks"]?[0]?["strAlcoholic"] ??
+                              "No alcoholic information found";
+                          image =
+                              myDrinks["drinks"]?[0]?["strDrinkThumb"] ?? "";
+                          isLoading = false;
+                        });
+                      },
+                      child: Text("click for new Cocktail"),
+                    ),
+                  ],
+                ),
         ),
-      )),
+      ),
     );
   }
 }
